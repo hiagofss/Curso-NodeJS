f91dd7980dad72f72da7dc367e8ddfa4dee3c129
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 19:55:41 -0300

    18_Models no Sequelize

--- Projeto01/test.js
+++ Projeto01/test.js
@@ -4,9 +4,42 @@ const sequelize = new Sequelize('sistemadecadastro', 'root', 'root', {
     dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function () {
-    console.log("Conectado com sucesso!")
-}).catch(function (erro) {
-    console.log("Falha ao se conectar: " + erro)
-    
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 })
+
+// Postagem.sync({force: true})
+
+// Postagem.create({
+//     titulo: "Um titulo qualquer",
+//     conteudo: "Conteudo qualquer"
+// })
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: "Hiago",
+    sobrenome: "Souza",
+    idade: 20,
+    email: "hiagofss98@gmail.com"
+})
+
+// Usuario.sync({force: true})
