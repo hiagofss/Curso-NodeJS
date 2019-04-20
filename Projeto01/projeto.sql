f8fc0a85ca0315d1ad9f96f1663f98177743d25b
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 18:12:41 -0300

    13_Mysql

--- Projeto01/projeto.sql
+++ Projeto01/projeto.sql
@@ -3,3 +3,8 @@ CREATE TABLE usuarios(
     email VARCHAR(100),
     idade INT
 );
+
+INSERT INTO usuarios(nome, email, idade) VALUES ("Hiago", "hiagofss98@gmail.com", 19);
+INSERT INTO usuarios(nome, email, idade) VALUES ("Lucas Silva", "email@teste22.com", 28);
+INSERT INTO usuarios(nome, email, idade) VALUES ("Lucas", "email@teste22.com", 39);
+INSERT INTO usuarios(nome, email, idade) VALUES ("Maria Clara", "maria@teste22.com", 8);
