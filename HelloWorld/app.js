ad932c1f59706b3de15fd04dd9195baeacc27a18
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 00:35:07 -0300

    Aula #03

---
+++ HelloWorld/app.js
@@ -0,0 +1,12 @@
+console.log("Hello World!")
+
+var nome = "Hiago"
+var sobrenome = "Souza"
+
+console.log(nome + " " + sobrenome)
+
+function somar(a, b) {
+    return a + b
+}
+
+console.log(somar(10, 10))
