5cc58b82f747ab716704ac41828520082165e495
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 12:07:06 -0300

    10_Exibindo HTML

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (red, res ) {
-    res.send("Seja bem vindo ao meu app!");
+app.get("/", function (red, res) {
+    res.sendFile(__dirname + "/html/index.html");
 });
 
-app.get("/sobre", function (red, res ) {
-    res.send("Minha pagina sobre!");
+app.get("/sobre", function (red, res) {
+    res.sendFile(__dirname + "/html/sobre.html");
 });
 
-app.get("/blog", function (red, res ) {
+app.get("/blog", function (red, res) {
     res.send("Bem vindo ao meu blog!");
 });
 
-app.get("/ola/:nome/:cargo/:cor", function (req, res ) {
-    res.send("<h1>Ola "+req.params.nome+"</h1>" + "<h2> Cargo "+req.params.cargo +"</h2>" + "<h3> Cor Favorita "+req.params.cor + "</h3>");
+app.get("/ola/:nome/:cargo/:cor", function (req, res) {
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Cargo " + req.params.cargo + "</h2>" + "<h3> Cor Favorita " + req.params.cor + "</h3>");
 });
-
 
 
 app.listen(8081, function () {
