6239dc554c2944c18292115dbcda55580a12d47d
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 19:24:49 -0300

    16_Sequelize

--- Projeto01/projeto.sql
+++ Projeto01/projeto.sql
@@ -8,3 +8,5 @@ INSERT INTO usuarios(nome, email, idade) VALUES ("Hiago", "hiagofss98@gmail.com"
 INSERT INTO usuarios(nome, email, idade) VALUES ("Lucas Silva", "email@teste22.com", 28);
 INSERT INTO usuarios(nome, email, idade) VALUES ("Lucas", "email@teste22.com", 39);
 INSERT INTO usuarios(nome, email, idade) VALUES ("Maria Clara", "maria@teste22.com", 8);
+
+DELETE FROM usuarios;
---
+++ Projeto01/test.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('sistemadecadastro', 'root', 'root', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function () {
+    console.log("Conectado com sucesso!")
+}).catch(function (erro) {
+    console.log("Falha ao se conectar: " + erro)
+    
+})
