3af2fdf95595893f0a165c81ca656f3d2668f3df
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 18:02:23 -0300

    11_12_Mysql

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -17,7 +17,6 @@ app.get("/ola/:nome/:cargo/:cor", function (req, res) {
     res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Cargo " + req.params.cargo + "</h2>" + "<h3> Cor Favorita " + req.params.cor + "</h3>");
 });
 
-
 app.listen(8081, function () {
     console.log("Servidor rodando na URL http://127.0.0.1:8081/")
 });
