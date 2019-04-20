b79341b308829be268a9a9b1b3b354b9f652074e
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 20:36:52 -0300

    21_Como enviar dados do formulário

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -4,20 +4,23 @@ const handlebars = require('express-handlebars')
 const Sequelize = require('sequelize')
 
 //Config
-//Template engine
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-//Conexao com o DB MySQL
-const sequelize = new Sequelize('sistemadecadastro', 'root', 'root', {
-    host: "localhost",
-    dialect: 'mysql'
-})
+    //Template engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    //Conexao com o DB MySQL
+    const sequelize = new Sequelize('sistemadecadastro', 'root', 'root', {
+        host: "localhost",
+        dialect: 'mysql'
+    })
 
 //Rotas
 app.get('/cad', function (req, res) {
     res.render('formulario');
 })
 
+app.post('/add', function (req, res) {
+    res.send("FORMULARIO RECEBIDO!")
+})
 
 app.listen(8081, function () {
     console.log("Servidor rodando na URL http://127.0.0.1:8081/")
