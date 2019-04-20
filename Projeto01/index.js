2cb965c405399b7d602eec7455f935d4f333cfcf
Hiago Souza <hiagofss98@gmail.com>
2019-04-20 20:08:08 -0300

    19_Handlebars

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
-app.get("/", function (red, res) {
-    res.sendFile(__dirname + "/html/index.html");
-});
+//Config
+    //Template engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    //Conexao com o DB MySQL
+    const sequelize = new Sequelize('sistemadecadastro', 'root', 'root', {
+        host: "localhost",
+        dialect: 'mysql'
+    })
 
-app.get("/sobre", function (red, res) {
-    res.sendFile(__dirname + "/html/sobre.html");
-});
-
-app.get("/blog", function (red, res) {
-    res.send("Bem vindo ao meu blog!");
-});
-
-app.get("/ola/:nome/:cargo/:cor", function (req, res) {
-    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Cargo " + req.params.cargo + "</h2>" + "<h3> Cor Favorita " + req.params.cor + "</h3>");
-});
 
 app.listen(8081, function () {
     console.log("Servidor rodando na URL http://127.0.0.1:8081/")
