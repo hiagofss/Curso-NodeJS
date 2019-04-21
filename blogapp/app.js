55b3d8b3b40509ad95040e3d7e330503f6e4177c
Hiago Souza <hiagofss98@gmail.com>
2019-04-21 20:33:43 -0300

    Middlewares #38

--- blogapp/app.js
+++ blogapp/app.js
@@ -26,12 +26,12 @@ mongoose.connect('mongodb://localhost/blogapp', {useNewUrlParser: true}).then(()
 
 //Public
 app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) =>{
+   console.log("Eu sou um middleware");
+    next()
+});
 
 //Rotas
-// app.use('/', (req, res) => {
-//     res.send("Rota principal")
-// });
-
 app.use('/posts', (req, res) => {
     res.send("Pagina de posts")
 });
