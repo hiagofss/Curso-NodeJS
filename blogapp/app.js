9f02f8eedb101957ce81384c6486fd47635a9c35
Hiago Souza <hiagofss98@gmail.com>
2019-04-21 19:35:38 -0300

    Cadastrando categorias no banco de dados #36

--- blogapp/app.js
+++ blogapp/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 // Configurações
 //Body Parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //Handlebars
