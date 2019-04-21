ec65cff9e9b8d98d7af7cd0e730bad546290ca44
Hiago Souza <hiagofss98@gmail.com>
2019-04-21 10:45:45 -0300

    Pegando dados do formulário #24

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Post = require('./models/Post')
 
 //Config
 //Template engine
@@ -12,13 +13,24 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 //Rotas
-app.get('/cad', function (req, res) {
-    res.render('formulario');
+app.get('/home', function (req, res) {
+    res.render('home');
 })
 
+app.get('/cad', function (req, res) {
+    res.render('formulario');
+});
+
 app.post('/add', function (req, res) {
-    res.send("Texto: " + req.body.titulo + " Conteudo: " + req.body.conteudo)
-})
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function () {
+        res.redirect('/home')
+    }).catch(function (erro) {
+        res.send("Houve um erro: " + erro)
+    })
+});
 
 app.listen(8081, function () {
     console.log("Servidor rodando na URL http://127.0.0.1:8081/")
