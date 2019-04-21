926e2884b5b6363bfbce42d53005ba474b0417f3
Hiago Souza <hiagofss98@gmail.com>
2019-04-21 19:25:20 -0300

    Definindo o model de categorias #35

--- blogapp/app.js
+++ blogapp/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const admin = require('./routes/admin');
 const path = require('path')
 
@@ -15,7 +15,14 @@ app.use(bodyParser.json());
 //Handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 //Mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/blogapp', {useNewUrlParser: true}).then(() => {
+    console.log("Conectado com sucesso!")
+}).catch((erro) => {
+    console.log("Houve um erro ao se conectar: " + erro)
+});
 
 //Public
 app.use(express.static(path.join(__dirname, "public")))
